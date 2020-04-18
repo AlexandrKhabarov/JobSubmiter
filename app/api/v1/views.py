4e8d123cb873672e783341458154b4c3555b9122
AlexandrKhabarov <rob.sumsung.mi3@gmail.com>
2020-04-18 20:57:45 +0300

    added more intelligent exception handling for status api

--- app/api/v1/views.py
+++ app/api/v1/views.py
@@ -4,6 +4,7 @@ import requests
 from flask import request
 from flask_restful import Resource
 from flask_restful.reqparse import RequestParser
+from requests import HTTPError, Timeout
 
 from app.utils.jenkins import Jenkins
 
@@ -54,17 +55,29 @@ class Status(Resource):
 
         jenkins = Jenkins(username, token)
         response = jenkins.job_info(job_name)
-        response = cls._create_response(response)
+        response = cls._create_response(response, job_name)
         return response
 
     @classmethod
-    def _create_response(cls, response):
+    def _create_response(cls, response, job_name):
         try:
             response.raise_for_status()
             content = json.loads(response.text)
-            res = ({"message": content['result'] or "RUNNING"}, response.status_code)
-
-        except requests.RequestException as e:
-            res = ({"message": str(e)}, e.response.status_code)
+            status = content['result']
+            status = status or "RUNNING"
+            res = ({"job_name": job_name, "status": status}, response.status_code)
+        except HTTPError as e:
+            if e.response.status_code in [401, 403]:
+                res = ({"message": 'Authentication failed'}, response.status_code)
+            elif e.response.status_code == 404:
+                res = ({"message": 'Requested item could not be found'}, response.status_code)
+            elif e.response.status_code >= 500:
+                res = ({"message": 'Something went wrong with Jenkins'}, response.status_code)
+            else:
+                res = ({"message": f'Client Error for job: {job_name}'}, response.status_code)
+        except Timeout:
+            res = ({"message": 'Request Timeout'}, response.status_code)
+        except ValueError:
+            res = ({"message": f"Could not parse JSON info for job: {job_name}"}, 500)
 
         return res
